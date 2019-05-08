efaacc4ecc916f8faa6e11cf145ddca069400314
Sunny Heyar <sunny.heyar@gmail.com>
2019-05-08 19:31:48 -0400

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Angus Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
