bec01b8451c207cb364b0045cd34aabea0060313
Sunny Heyar <sunny.heyar@gmail.com>
2019-05-14 18:13:58 -0400

    added jawsdb env variable

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin123",
-  port: 3306,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin123",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(err => {
   if (err) {
